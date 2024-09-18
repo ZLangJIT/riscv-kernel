98be278bb42a9535ad1d44805d161e5d2504beb0
root <root@localhost.localdomain>
2024-09-18 05:15:20 +0000

    update

--- push.sh
+++ push.sh
@@ -1,1 +1,14 @@
-git add -Av ; git commit -m "update" ; git tag -f -a -m "riscv-kernel" riscv-kernel-6.11 ; git log -n 1 ; git push --follow-tags
+#
+#
+# kernel release version
+#
+# increment this each time we push a change or the CI will not build it if the CURRENT release matches
+#
+# eg, if we successfully build and release TAG=6.11.0
+#   then attempting to build TAG=6.11.0 again will no-op even if we push changes
+#   if we build and release 6.11.1 then building 6.11.0 will succeed since 6.11.0 != 6.11.1
+#
+#
+TAG=6.11.0
+
+git tag -a -m "riscv-kernel" riscv-kernel-$TAG && git add -Av && git commit -m "update" &&  git log -n 1 && git push --follow-tags
