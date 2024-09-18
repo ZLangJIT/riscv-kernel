3372707608e7f7e0d5e31c91fa855f99a622d9cf
root <root@localhost.localdomain>
2024-09-18 11:41:11 +0000

    update to riscv-kernel-6.11.16

--- boot.sh
+++ boot.sh
@@ -1,12 +1,9 @@
+#
 # to exit via terminal - CTRL-A + x
 #
 # to exit via kernel shell - busybox poweroff -f
 #
-#./make.sh
-#./debug_BUILD/rvvm ./uboot -v -k ./riscv-linux-prebuilt/kernel/vmlinux-rv64-5.4-rc7 -m 100m
 
-TAG=$(grep 'LOCALVERSION=' riscv_defconfig | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
-set -x
-wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/vmlinux-riscv64.ramdisk.gz &&
-wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/vmlinux-riscv64 &&
-../RVVM/debug_BUILD/rvvm ../RVVM/uboot -v -k vmlinux-riscv64 -i vmlinux-riscv64.ramdisk.gz -m 100m
+../RVVM/debug_BUILD/rvvm --help
+../RVVM/debug_BUILD/rvvm ../RVVM/uboot -v -k vmlinux-riscv64 -m 100m \
+  -cmdline="console=ttyS rootflags=discard rw $1"
