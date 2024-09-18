92bfc711f25466068b42dcf71cebe013f9a36c0e
root <root@localhost.localdomain>
2024-09-18 04:44:26 +0000

    update

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-git add -Av ; git commit -m "update" ; git tag -f riscv-kernel-6.11 ; git log -n 1 ; git push
+git add -Av ; git commit -m "update" ; git tag -f riscv-kernel-6.11 ; git log -n 1 ; git push --tags
