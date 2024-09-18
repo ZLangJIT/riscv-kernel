b2a51f8c9df43d5a4fbd9404a5ac41ade8aca588
root <root@localhost.localdomain>
2024-09-18 08:58:33 +0000

    update to riscv-kernel-6.11.15

--- boot.sh
+++ boot.sh
@@ -7,6 +7,6 @@
 
 TAG=$(grep 'LOCALVERSION=' riscv_defconfig | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
 set -x
-wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG.1/vmlinux-riscv64 &&
-wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG.1/vmlinux-riscv64.ramdisk.gz &&
+wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/vmlinux-riscv64.ramdisk.gz &&
+wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/vmlinux-riscv64 &&
 ../RVVM/debug_BUILD/rvvm ../RVVM/uboot -v -k vmlinux-riscv64 -i vmlinux-riscv64.ramdisk.gz -m 100m
