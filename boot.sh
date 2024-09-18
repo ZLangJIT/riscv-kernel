ef19a17db7e5068518242f8e866d30e06348bed7
root <root@localhost.localdomain>
2024-09-18 07:07:13 +0000

    update to riscv-kernel-6.11.9

---
+++ boot.sh
@@ -0,0 +1,11 @@
+# to exit via terminal - CTRL-A + x
+#
+# to exit via kernel shell - busybox poweroff -f
+#
+#./make.sh
+#./debug_BUILD/rvvm ./uboot -v -k ./riscv-linux-prebuilt/kernel/vmlinux-rv64-5.4-rc7 -m 100m
+
+TAG=$(grep 'LOCALVERSION=' riscv_defconfig | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
+set -x
+wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/vmlinux-riscv64 &&
+../RVVM/debug_BUILD/rvvm ../RVVM/uboot -v -k vmlinux-riscv64 -m 100m
