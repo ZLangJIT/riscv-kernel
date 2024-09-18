d547aee0712ffdef41302fb510aede7defa1f00a
root <root@localhost.localdomain>
2024-09-18 04:22:55 +0000

    update files

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ fi
 #
 if [ "$IS_LTS" = "NO" ]; then
 	echo -e "Using $LOGICAL_CORES jobs for this non-LTS build..."
-	make CC='ccache clang -Qunused-arguments -fcolor-diagnostics' LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
+	make CC='ccache clang -Qunused-arguments -fcolor-diagnostics' ARCH=riscv LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
 else
 	echo -e "Using $LOGICAL_CORES jobs for this LTS build..."
-	make LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
+	make ARCH=riscv LLVM=1 LLVM_IAS=1 -j$LOGICAL_CORES
 fi
