c5c16953ff24e0c318b97f58ed963191905f1c41
root <root@localhost.localdomain>
2024-09-20 12:13:57 +0000

    update to riscv-kernel-6.11.23

--- boot_download.sh
+++ boot_download.sh
@@ -1,4 +1,4 @@
 TAG=$(grep 'LOCALVERSION=' riscv_defconfig | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
 set -x
-rm Image
-wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/Image
+rm Image.gz
+wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/Image.gz
