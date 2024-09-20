afe458326d8ac18369393ff06d7f33e64ad2e6c2
root <root@localhost.localdomain>
2024-09-20 00:37:15 +0000

    update to riscv-kernel-6.11.22

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 
-if [ "$(cat /sys/devices/system/cpu/smt/active)" = "1" ]; then
-	export LOGICAL_CORES=$(($(nproc --all) * 2))
-else
-	export LOGICAL_CORES=$(nproc --all)
-fi
+. cores.sh
+
 # Compile
 #
 
