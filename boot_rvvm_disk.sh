c8186c0cdcdfba41cfbcef73e7117e594b2f6205
root <root@localhost.localdomain>
2024-09-30 15:28:37 +0000

    update to riscv-kernel-6.11.35

--- boot_rvvm_disk.sh
+++ boot_rvvm_disk.sh
@@ -5,5 +5,5 @@ fi
 
 dd if=/dev/zero of=disk.img bs=1M count=100
 
-../RVVM/debug_BUILD/rvvm ../RVVM/uboot -v -k Image -m 100m -cmdline="console=ttyS0 rootflags=discard rw $1" -k disk.img $2
-# gdb --args ./rvvm ../RVVM/uboot -v -k Image -m 100m -cmdline="console=ttyS0 rootflags=discard rw $1" -k disk.img $2
+# ../RVVM/debug_BUILD/rvvm ../RVVM/uboot -v -k Image -m 100m -cmdline="console=ttyS0 rootflags=discard rw $1" -k disk.img $2
+gdb --args ./rvvm ../RVVM/uboot -v -k Image -m 100m -cmdline="console=ttyS0 rootflags=discard rw $1" -k disk.img $2
