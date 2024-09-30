4f4cc6ebaa720de3e34b3d7f870532a0a487c4c1
root <root@localhost.localdomain>
2024-09-30 13:27:12 +0000

    update to riscv-kernel-6.11.33

--- gen_patch__libmedia.sh
+++ gen_patch__libmedia.sh
@@ -2,4 +2,6 @@ set -x
 touch libmedia.patch
 cd libmedia
 git reset 0f06c20bf1341da28213178545ba056f1791896f
+mv app/src/main/java/libengine/RVVM/.git app/src/main/java/libengine/RVVM/.git0
 git add -Av && git commit -m "c" && git format-patch --progress --stdout -n1 > ../libmedia.patch
+mv app/src/main/java/libengine/RVVM/.git0 app/src/main/java/libengine/RVVM/.git
