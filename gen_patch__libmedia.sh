eff47c393544a05ed4ccf850e2cb7652348a8df1
root <root@localhost.localdomain>
2024-10-01 06:02:53 +0000

    update to riscv-kernel-6.11.36

--- gen_patch__libmedia.sh
+++ gen_patch__libmedia.sh
@@ -1,4 +1,5 @@
 ./gen_patch__rvvm.sh
+./gen_patch__virgl.sh
 set -x
 if [[ -f libmedia.patch ]]
 	then
@@ -7,12 +8,25 @@ fi
 touch libmedia.patch
 cd libmedia
 git reset 0f06c20bf1341da28213178545ba056f1791896f
+
 mv app/src/main/java/libengine/RVVM/.git app/src/main/java/libengine/RVVM/.git0
 mv app/src/main/java/libengine/RVVM.reset/.git app/src/main/java/libengine/RVVM.reset/.git0
 mv app/src/main/java/libengine/RVVM ../RVVM.current
 mv app/src/main/java/libengine/RVVM.reset app/src/main/java/libengine/RVVM
+
+mv app/src/main/java/libengine/virglrenderer/.git app/src/main/java/libengine/virglrenderer/.git0
+mv app/src/main/java/libengine/virglrenderer.reset/.git app/src/main/java/libengine/virglrenderer.reset/.git0
+mv app/src/main/java/libengine/virglrenderer ../virglrenderer.current
+mv app/src/main/java/libengine/virglrenderer.reset app/src/main/java/libengine/virglrenderer
+
 git add -Av && git commit -m "c" && git format-patch --progress --stdout -n1 > ../libmedia.patch
+
 mv app/src/main/java/libengine/RVVM app/src/main/java/libengine/RVVM.reset
 mv ../RVVM.current app/src/main/java/libengine/RVVM
 mv app/src/main/java/libengine/RVVM.reset/.git0 app/src/main/java/libengine/RVVM.reset/.git
 mv app/src/main/java/libengine/RVVM/.git0 app/src/main/java/libengine/RVVM/.git
+
+mv app/src/main/java/libengine/virglrenderer app/src/main/java/libengine/virglrenderer.reset
+mv ../virglrenderer.current app/src/main/java/libengine/virglrenderer
+mv app/src/main/java/libengine/virglrenderer.reset/.git0 app/src/main/java/libengine/virglrenderer.reset/.git
+mv app/src/main/java/libengine/virglrenderer/.git0 app/src/main/java/libengine/virglrenderer/.git
---
+++ gen_patch__virgl.sh
@@ -0,0 +1,9 @@
+set -x
+if [[ -f virglrenderer.patch ]]
+	then
+		rm -v virglrenderer.patch
+fi
+touch virgl.patch
+cd libmedia/app/src/main/java/libengine/virglrenderer
+git reset 2e38df33fb02e5635bbca8189e14eb15fc4f4a9c
+git add -Av && git commit -m "c" && git format-patch --progress --stdout -n1 > ../../../../../../../virglrenderer.patch
