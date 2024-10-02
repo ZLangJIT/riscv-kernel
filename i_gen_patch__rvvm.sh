1fcdbfc943dd807f4d01c3b02e125b5057ff896d
root <root@localhost.localdomain>
2024-10-02 15:06:26 +0000

    update to riscv-kernel-6.11.39

--- i_gen_patch__rvvm.sh
+++ i_gen_patch__rvvm.sh
@@ -5,5 +5,5 @@ if [[ -f rvvm.patch ]]
 fi
 touch rvvm.patch
 cd libmedia/app/src/main/java/libengine/RVVM
-git reset 879138053b941ce5ea79efbd5fc3b368523b05b0
+git reset 2a5d6744e0261c5b63db218d41c73aab32416cc2
 git add -Av && git commit -m "c" && git format-patch --progress --stdout -n1 > ../../../../../../../rvvm.patch
