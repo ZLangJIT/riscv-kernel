fc7db571d9a62ebbd957c91cc33858d92bf54554
root <root@localhost.localdomain>
2024-10-02 08:42:41 +0000

    update to riscv-kernel-6.11.37

--- gen_patch__buildroot.sh
+++ gen_patch__buildroot.sh
@@ -5,5 +5,5 @@ if [[ -f buildroot.patch ]]
 fi
 touch buildroot.patch
 cd buildroot
-git reset db37b0e27d3c54d27b854edbc3253d29f512a6ee
+git reset 8d835ffc524e2dab66ce1421240b9eb93c8f8f6a
 git add -Av && git commit -m "c" && git format-patch --progress --stdout -n1 > ../buildroot.patch
