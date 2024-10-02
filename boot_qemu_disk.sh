90c7411e4eb18ec9d114b002d44acd7385292dce
root <root@localhost.localdomain>
2024-10-02 22:11:28 +0000

    update to riscv-kernel-6.11.39

--- boot_qemu_disk.sh
+++ boot_qemu_disk.sh
@@ -5,5 +5,5 @@ fi
 
 dd if=/dev/zero of=disk.img bs=1M count=100
 
-qemu-system-riscv64 -nographic -machine virt -kernel Image --append "console=ttyS0 rootflags=discard rw $1" \
+qemu-system-riscv64 -nographic -machine virt -m 500m -kernel Image --append "console=ttyS0 rootflags=discard rw $1" \
 -drive file=disk.img,format=raw,if=none,id=nvm -device nvme,serial=deadbeef,drive=nvm $2
