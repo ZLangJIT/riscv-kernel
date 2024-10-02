6679b5247beebebda02a5ab12b88193a26a5379d
root <root@localhost.localdomain>
2024-10-02 15:16:42 +0000

    update to riscv-kernel-6.11.39

--- apk_download.sh
+++ apk_download.sh
@@ -1,14 +1,14 @@
 TAG=$(grep 'LOCALVERSION=' riscv_defconfig | sed 's/^.*[^0-9]\([0-9]*\.[0-9]*\.[0-9]*\).*$/\1/')
 set -x
 cd /data/data/com.termux/files/home
-if [[ -f linux.kernel.rvvm.debug.apk ]]
+if [[ -f linux.kernel.rvvm.release.apk ]]
 	then
-		rm -v linux.kernel.rvvm.debug.apk
+		rm -v linux.kernel.rvvm.release.apk
 fi
-wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/linux.kernel.rvvm.debug.apk &&
+wget https://github.com/ZLangJIT/riscv-kernel/releases/download/$TAG/linux.kernel.rvvm.release.apk &&
 set +x &&
 echo &&
 echo 'please install the following apk ...' &&
 echo &&
-echo '/data/data/com.termux/files/home/linux.kernel.rvvm.debug.apk' &&
+echo '/data/data/com.termux/files/home/linux.kernel.rvvm.release.apk' &&
 echo
