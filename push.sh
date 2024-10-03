73c381108f6c5d1161a2e1ca1de862ea553a6590
root <root@localhost.localdomain>
2024-10-03 06:10:15 +0000

    update to riscv-kernel-6.11.0 v5

--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 . ./compute_libmedia_version.sh
 
-git add -Av ; git commit -m "update to riscv-kernel-6.11.0 with version code $LIBMEDIA_GRADLE_VERSION_CODE" ; git log -n 1 ; git push
+git add -Av ; git commit -m "update to riscv-kernel-6.11.0 v$LIBMEDIA_GRADLE_VERSION_CODE" ; git log -n 1 ; git push
