f7583e1d9068b5b0f5280e4621363836ac2de0b1
ZLangJIT <root@no.email.yet.com>
2024-10-05 09:01:58 +0000

    update to riscv-kernel-6.11.0 v6

--- gen_patch__libmedia.sh
+++ gen_patch__libmedia.sh
@@ -1,7 +1,43 @@
+set -v
+
+function save() {
+    mv app/src/main/java/libengine/$1/.git app/src/main/java/libengine/$1/.git0
+    mv app/src/main/java/libengine/$1.reset/.git app/src/main/java/libengine/$1.reset/.git0
+    mv app/src/main/java/libengine/$1 ../$1.current
+    mv app/src/main/java/libengine/$1.reset app/src/main/java/libengine/$1
+}
+
+function load() {
+    mv app/src/main/java/libengine/$1 app/src/main/java/libengine/$1.reset
+    mv ../$1.current app/src/main/java/libengine/$1
+    mv app/src/main/java/libengine/$1.reset/.git0 app/src/main/java/libengine/$1.reset/.git
+    mv app/src/main/java/libengine/$1/.git0 app/src/main/java/libengine/$1/.git
+}
+
+set -x
+
+if [[ -d epoxy.current ]]
+  then
+    cd libmedia
+    load epoxy
+    cd ..
+fi
+if [[ -d virglrenderer.current ]]
+  then
+    cd libmedia
+    load virglrenderer
+    cd ..
+fi
+if [[ -d RVVM.current ]]
+  then
+    cd libmedia
+    load RVVM
+    cd ..
+fi
+
 ./i_gen_patch__epoxy.sh
 ./i_gen_patch__virglrenderer.sh
 ./i_gen_patch__rvvm.sh
-set -x
 if [[ -f libmedia.patch ]]
 	then
 		rm -v libmedia.patch
@@ -11,19 +47,6 @@ echo "patch file" > libmedia.patch
 cd libmedia
 git reset $R
 
-function save() {
-mv app/src/main/java/libengine/$1/.git app/src/main/java/libengine/$1/.git0
-mv app/src/main/java/libengine/$1.reset/.git app/src/main/java/libengine/$1.reset/.git0
-mv app/src/main/java/libengine/$1 ../$1.current
-mv app/src/main/java/libengine/$1.reset app/src/main/java/libengine/$1
-}
-
-function load() {
-mv app/src/main/java/libengine/$1 app/src/main/java/libengine/$1.reset
-mv ../$1.current app/src/main/java/libengine/$1
-mv app/src/main/java/libengine/$1.reset/.git0 app/src/main/java/libengine/$1.reset/.git
-mv app/src/main/java/libengine/$1/.git0 app/src/main/java/libengine/$1/.git
-}
 
 save epoxy
 save virglrenderer
--- push.sh
+++ push.sh
@@ -1,3 +1,7 @@
+if [[ $1 == '-v' ]]
+ then
+   IS_VERBOSE="--show-progress -v"
+fi
 . ./compute_libmedia_version.sh
 
 git add -Av ; git commit -m "update to riscv-kernel-6.11.0 v$LIBMEDIA_GRADLE_VERSION_CODE" ; git log -n 1 ; git push
