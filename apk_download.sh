560b1f0267d980fecf44a5079f8deb88e7b5b0da
ZLangJIT <root@no.email.yet.com>
2024-10-06 19:15:34 +0000

    update to riscv-kernel-6.11.0 v16

--- apk_download.sh
+++ apk_download.sh
@@ -1,4 +1,33 @@
-. ./compute_libmedia_version.sh
+IS_VERBOSE="-q"
+while [[ $# -gt '0' ]]
+  do
+    if [[ "$1" == '-v' ]]
+      then
+        IS_VERBOSE="--show-progress -q"
+        shift
+        if [[ $# -gt '0' ]]
+          then
+            if [[ "$1" == '-v' ]]
+              then
+                IS_VERBOSE="--show-progress -v"
+                shift
+            fi
+        fi
+    else
+      if [[ "$1" == '--version_code' ]]
+        then
+          LIBMEDIA_GRADLE_VERSION_CODE="$2"
+          shift
+          shift
+        else
+          shift
+      fi
+  fi
+done
+if [[ -z $LIBMEDIA_GRADLE_VERSION_CODE ]]
+  then
+    . ./compute_libmedia_version.sh
+fi
 TAG=$LIBMEDIA_GRADLE_VERSION_CODE
 
 if $(am --help 2>&1 | grep -q -i "to-intent-uri")
