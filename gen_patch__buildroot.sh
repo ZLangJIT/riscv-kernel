5872e92b7ce52bcf23ad51fde7029b287f6f57c1
ZLangJIT <root@no.email.yet.com>
2024-10-30 01:27:37 +0000

    update to riscv-kernel-6.11.0 v41

--- gen_patch__buildroot.sh
+++ gen_patch__buildroot.sh
@@ -6,6 +6,9 @@ fi
 R=$(cat git_reset_buildroot)
 echo "patch file" > buildroot.patch
 cd buildroot
+clang ../t.c -o t || exit 1
+./t ../.buildrootconfig > dot_buildrootconfig
+rm t
 git reset $R
 git add -AN
 git diff --binary $R >> ../buildroot.patch
